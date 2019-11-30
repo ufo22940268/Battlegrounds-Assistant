79a1c1e192687b0749a2f4ccf3784fc88494ee98
Frank Cheng <ufo22940268@gmail.com>
2019-11-30 10:56:58 +0800

    v

--- t/CardDetail.swift
+++ t/CardDetail.swift
@@ -9,20 +9,53 @@
 import SwiftUI
 import KingfisherSwiftUI
 
+struct Stars: View {
+    
+    var num: Int
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 10) {
+            ForEach(0..<num) { _ in
+                Image(systemName: "star.fill")
+                .resizable()
+                    .frame(width: 30, height: 30, alignment: .leading)
+                    .foregroundColor(.yellow)
+            }
+            if num < 5 {
+                ForEach(0..<5 - num) { _ in
+                    Image(systemName: "star")
+                    .resizable()
+                        .frame(width: 30, height: 30, alignment: .leading)
+                        .foregroundColor(.yellow)
+                }
+            }
+            Spacer()
+        }.padding(20.0)
+    }
+}
+
 struct CardDetail: View {
     
     let card: Card
     
     var body: some View {
         VStack {
+            Stars(num: 3)
             KFImage(URL(string: card.image))
-        }.navigationBarTitle(card.name)
+                .resizable(capInsets: EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 30), resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .padding(.horizontal, 27.482)
+                .padding(.top, 20)
+            Spacer()
+        }
+        .navigationBarTitle(card.name)
     }
 }
 
 struct CardDetail_Previews: PreviewProvider {
     static var previews: some View {
-        CardDetail(card: cardData.first!)
-//            .environment(\.colorScheme, .dark)
+        NavigationView {
+            CardDetail(card: cardData.first!)
+        }
     }
 }
